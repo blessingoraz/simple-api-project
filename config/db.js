2741571311a9d198eea2b1180641850508e59773
Blessing Orazulume <blessing.orazulume@andela.com>
2018-02-15 11:31:10 +0100

    updated mongolab config on heroku

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const USERNAME= process.env.USERNAME;
-const PASSWORD= process.env.PASSWORD;
 module.exports = {
-    url: `mongodb://${USERNAME}:${PASSWORD}@ds235768.mlab.com:35768/note-taking`
+    url: process.env.MONGODB_URI
 };

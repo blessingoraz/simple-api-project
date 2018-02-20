6ddd379fdc4f18bcf5ffaee68adda06357d87c17
Blessing Orazulume <blessing.orazulume@andela.com>
2018-02-20 12:18:29 +0100

    minor fix

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    req.header("Access-Control-Allow-Origin", "GET, POST, OPTIONS, PUT, DELETE");
+    req.headers("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
 });
 

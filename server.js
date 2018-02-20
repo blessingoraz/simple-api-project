2e2f56476ee0a01997675d2865e1c3ced423338a
Blessing Orazulume <blessing.orazulume@andela.com>
2018-02-20 10:12:25 +0100

    CORS fix

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ dbConn.once('open', () => {
     console.log('DB is connected')
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to Notes API skrrrr'})
+    res.json({ message: 'Welcome to Notes API skrrrr' })
 });
 
 // add routes here

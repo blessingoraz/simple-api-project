d12cf1dfbb5541b342e34299dcc86c1a2a3e9cf7
Blessing Orazulume <blessing.orazulume@andela.com>
2018-02-20 12:06:47 +0100

    minor fix

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    req.header("Access-Control-Allow-Origin", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
